258eabd32e40ffa9226f71b5d7d8db220a5e26cf
Olga Shomarova <olga.shomarova@protonmail.com>
2026-03-20 11:08:13 +0200

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,60 @@
+pipeline {
+    agent any
+     environment {
+            JAVA_HOME = '/usr/lib/jvm/java-21-openjdk'
+            DOCKERHUB_CREDENTIALS_ID = 'dockerhub-credentials'
+            DOCKERHUB_REPO = 'olgasho/temperatureconverter'
+            DOCKER_IMAGE_TAG = 'latest'
+         }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                url: 'https://github.com/olshom/TemperatureConverter.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'mvn clean install' // sh for linux and ios
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Code Coverage') {
+            steps {
+                sh 'mvn jacoco:report'
+            }
+        }
+        stage('Publish Test Results') {
+            steps {
+                junit '**/target/surefire-reports/*.xml'
+            }
+        }
+        stage('Publish Coverage Report') {
+            steps {
+                recordCoverage(tools: [[parser: 'JACOCO']])
+            }
+        }
+        stage('Build Docker Image') {
+                  steps {
+                      script {
+                          docker.build("${DOCKERHUB_REPO}:${DOCKER_IMAGE_TAG}")
+                      }
+                  }
+              }
+
+              stage('Push Docker Image to Docker Hub') {
+                  steps {
+                      script {
+                          docker.withRegistry('https://index.docker.io/v1/', DOCKERHUB_CREDENTIALS_ID) {
+                              docker.image("${DOCKERHUB_REPO}:${DOCKER_IMAGE_TAG}").push()
+                          }
+                      }
+                  }
+              }
+    }
+}
---
+++ src/main/java/ShoppingCart.java
@@ -0,0 +1,53 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class ShoppingCart {
+    public static Locale getLocale(int choice) {
+        switch (choice) {
+            case 1: return new Locale("en", "US");
+            case 2: return new Locale("fi", "FI");
+            case 3: return new Locale("sv", "SE");
+            case 4: return new Locale("ja", "JP");
+            default: return new Locale("en", "US");
+        }
+    }
+
+    public static int calculateTotal(int[] prices, int[] quantities) {
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            result += prices[i] * quantities[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Select a language: ");
+        System.out.println("1. English");
+        System.out.println("2. Finnish");
+        System.out.println("3. Swedish");
+        System.out.println("4. Japanese");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        if (choice < 1 || choice > 4) {
+            System.out.println("Invalid choice. Defaulting to English.");
+        }
+        Locale locale = getLocale(choice);
+
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("MessagesBundle", locale);
+        System.out.println(resourceBundle.getString("prompt1"));
+        int numberOfItems = scanner.nextInt();
+
+        int[] prices = new int[numberOfItems];
+        int[] quantities = new int[numberOfItems];
+        for (int i = 0; i < numberOfItems; i++) {
+            System.out.println(resourceBundle.getString("prompt2"));
+            prices[i] = scanner.nextInt();
+            System.out.println(resourceBundle.getString("prompt3"));
+            quantities[i] = scanner.nextInt();
+        }
+
+        System.out.println(resourceBundle.getString("prompt4") + calculateTotal(prices, quantities));
+    }
+}
